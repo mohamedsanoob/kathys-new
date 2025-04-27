98f65eaa0eb50a8ac9901783662a53ede4a47ee2
edwinjzph <edwinjzph2000@gmail.com>
2025-04-27 13:23:07 +0530

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
       </div>
         }>
            <AuthProvider>
-          <div className="flex flex-col h-screen">
+          <div className="flex flex-col h-[94vh]">
             <Navbar />
             <div className="md:pb-0 overflow-y-auto flex-1">
               {children}
