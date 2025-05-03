8f30459dbd433191ce64163f10cc9dab2f2134e4
edwinjzph <edwinjzph2000@gmail.com>
2025-05-03 21:04:03 +0530

    fix

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -500,7 +500,7 @@ const CheckoutPage = () => {
             onClick={handleOrderButtonClick}
             disabled={
               (currentUser 
-                ? !selectedAddress || !termsAgreed || !showPaymentMode 
+                ? !selectedAddress || !termsAgreed || !showAddressForm
                 : !isValid || !termsAgreed) || isProcessingPayment
             }
             className={`w-full py-3 rounded-md text-white font-semibold ${
