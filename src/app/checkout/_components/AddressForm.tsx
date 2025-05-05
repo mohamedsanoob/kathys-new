e3ceae6bf7307e6a55fe6a17799d166f6ef7b7cd
edwinjzph <edwinjzph2000@gmail.com>
2025-05-06 01:50:36 +0530

    fix

--- src/app/checkout/_components/AddressForm.tsx
+++ src/app/checkout/_components/AddressForm.tsx
@@ -173,7 +173,7 @@ const AddressForm = ({ register, errors, onSubmit }: AddressFormProps) => {
 
       <button
         type="submit"
-        className="w-full py-3  text-white font-medium rounded bg-bg-[#1e6553] hover:bg-bg-[#1e6553] transition-colors"
+        className="w-full py-3  text-white font-medium rounded bg-[#1e6553] hover:bg-[#1e6553] transition-colors"
         style={{cursor:"pointer"}}
       >
         Save Address
