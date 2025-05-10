2f9991c759567de3d5a4c8fec1c35d830068a305
edwinjzph <edwinjzph2000@gmail.com>
2025-05-10 21:41:52 +0530

    track order remove mt 6

--- src/app/track-order/[id]/page.tsx
+++ src/app/track-order/[id]/page.tsx
@@ -61,7 +61,7 @@ const Page = () => {
   const grandTotal = (itemsTotal + deliveryFee).toFixed(2);
 
   return (
-    <div className="max-w-2xl mx-auto p-4 sm:p-6 bg-white rounded-lg shadow-md mt-6">
+    <div className="max-w-2xl mx-auto p-4 sm:p-6 bg-white rounded-lg shadow-md">
       <div className="mb-4">
         <Link
           href={`${user.currentUser?'/account?category=orders':'/'}`}
