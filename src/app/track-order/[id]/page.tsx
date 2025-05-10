c669a059c24a78440c4a8eb57dd82d2b4664636d
edwinjzph <edwinjzph2000@gmail.com>
2025-05-10 21:44:07 +0530

    fix

--- src/app/track-order/[id]/page.tsx
+++ src/app/track-order/[id]/page.tsx
@@ -61,7 +61,7 @@ const Page = () => {
   const grandTotal = (itemsTotal + deliveryFee).toFixed(2);
 
   return (
-    <div className="max-w-2xl mx-auto p-4 sm:p-6 bg-white rounded-lg shadow-md md:my-4">
+    <div className="max-w-2xl mx-auto p-4 sm:p-6 bg-white rounded-lg shadow-md my-1 md:my-4">
       <div className="mb-4">
         <Link
           href={`${user.currentUser?'/account?category=orders':'/'}`}
