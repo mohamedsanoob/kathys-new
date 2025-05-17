5a958aeab1c22722ab4dec93c717833e0933bafd
edwinjzph <edwinjzph2000@gmail.com>
2025-05-17 16:02:09 +0530

    fix

--- src/app/product/[product_id]/_components/ProductDescription.tsx
+++ src/app/product/[product_id]/_components/ProductDescription.tsx
@@ -16,6 +16,7 @@ const ProductDescription = ({
   variants: Variants[];
 }) => {
   const [tab, setTab] = useState("description");
+  const hasVariants = variants && variants.length > 0;
 
   return (
     <div className="flex flex-col md:gap-4 w-[90%] md:w-full m-auto">
@@ -31,26 +32,19 @@ const ProductDescription = ({
         >
           Description
         </button>
-        <button
-          onClick={() => setTab("information")}
-          className={`whitespace-nowrap text-lg md:text-2xl font-medium px-2 py-1 rounded-lg transition-colors ${
-            tab === "information"
-              ? "text-black bg-gray-100"
-              : "text-gray-500 hover:text-gray-700"
-          }`}
-        >
-          Information
-        </button>
-        {/* <button
-          onClick={() => setTab("reviews")}
-          className={`whitespace-nowrap text-lg md:text-2xl font-medium px-2 py-1 rounded-lg transition-colors ${
-            tab === "reviews"
-              ? "text-black bg-gray-100"
-              : "text-gray-500 hover:text-gray-700"
-          }`}
-        >
-          Reviews
-        </button> */}
+        
+        {hasVariants && (
+          <button
+            onClick={() => setTab("information")}
+            className={`whitespace-nowrap text-lg md:text-2xl font-medium px-2 py-1 rounded-lg transition-colors ${
+              tab === "information"
+                ? "text-black bg-gray-100"
+                : "text-gray-500 hover:text-gray-700"
+            }`}
+          >
+            Information
+          </button>
+        )}
       </div>
 
       {/* Divider */}
@@ -61,13 +55,6 @@ const ProductDescription = ({
         {tab === "information" ? (
           <Information variants={variants} />
         ) : (
-          // ) : tab === "reviews" ? (
-          //   <div className="text-gray-700">
-          //     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          //     malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
-          //     dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
-          //     lacus ex, sit amet
-          //   </div>
           <Description description={description} />
         )}
       </div>
