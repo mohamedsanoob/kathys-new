58c6bde6d9c0b4904c403a20432ed60fc223dbd0
edwinjzph <edwinjzph2000@gmail.com>
2025-09-27 15:05:21 +0530

    ads sense fix

--- src/app/_components/AdBanner.tsx
+++ src/app/_components/AdBanner.tsx
@@ -27,7 +27,7 @@ const AdBanner = ({
     <ins
       className="adsbygoogle"
       style={{ display: "block" }}
-      data-ad-client="ca-pub-123456789"
+      data-ad-client="ca-pub-8258677943197720"
       data-ad-slot={dataAdSlot}
       data-ad-format={dataAdFormat}
       data-full-width-responsive={dataFullWidthResponsive.toString()}
